f2c6d87f31c8ece1c4f1bc41f997366a78a3be56
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-29 23:45:18 +0600

    added max.js and max3.js files

---
+++ max3.js
@@ -0,0 +1,32 @@
+const jim = 256;
+const tim = 389;
+const kim = 168;
+
+if (jim > tim && jim > kim) {
+    console.log('Jim is the ultimate boss');
+}
+else if (tim > jim && tim > kim) {
+    console.log('Tim is the boss');
+}
+else {
+    console.log('Kim is the kardashians boss');
+}
+
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    }
+    else if (num2 > num1 && num2 > num3) {
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+
+const max1 = maxOfThree(127, 546, 78);
+console.log(max1);
+
+
+const max2 = Math.max(12, 1, 56, 5, 65, 8, 1, 99, 2);
+console.log('max using Math.max', max2);
