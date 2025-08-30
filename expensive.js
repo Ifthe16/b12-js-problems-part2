37ed46f5d3378d433f57cd1883725454ce0aaf51
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-30 06:21:09 +0600

    added cheapest.js and expensive.js files and updated README.md

---
+++ expensive.js
@@ -0,0 +1,21 @@
+const phones = [
+    { name: 'Samsung', price: 20000, camera: '12mp', color: 'black' },
+    { name: 'xoami', price: 18000, camera: '12mp', color: 'black' },
+    { name: 'Oppo', price: 30000, camera: '12mp', color: 'black' },
+    { name: 'Iphone', price: 100000, camera: '12mp', color: 'black' },
+    { name: 'Walton', price: 31000, camera: '12mp', color: 'black' },
+    { name: 'HTC', price: 37000, camera: '12mp', color: 'black' },
+];
+
+function getExpensivePhone(phones) {
+    let max = phones[0];
+    for (const phone of phones) {
+        if (phone.price > max.price) {
+            max = phone;
+        }
+    }
+    return max.name;
+}
+
+const expensive = getExpensivePhone(phones);
+console.log('Expensive phone is:', expensive);
